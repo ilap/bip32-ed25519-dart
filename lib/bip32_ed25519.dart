279530a2d1e7124232784cd0b8467244a0791307
Pal Dorogi <pal.dorogi@gmail.com>
2020-12-21 17:09:16 +1000

    Fixed Signing.

--- lib/bip32_ed25519.dart
+++ lib/bip32_ed25519.dart
@@ -1,15 +1,14 @@
 import 'dart:typed_data';
 import 'package:pinenacl/api.dart';
 
-mixin ExtendedKey {
+mixin ExtendedKey on ByteList {
   ExtendedKey get public;
-  ByteList get chainCode;
+  ByteList get chainCode => suffix;
   ExtendedKey derive(int index);
 }
 
 mixin Bip32 {
   ExtendedKey get master;
-  ExtendedKey derive(int index);
   ExtendedKey fromPath(String path);
 }
 
@@ -70,7 +69,7 @@ class CardanoExtendedPrivateKey extends ByteList
     // signed message
     var sm = Uint8List(message.length + TweetNaCl.signatureLength);
     final result = TweetNaCl.crypto_sign(
-        sm, -1, Uint8List.fromList(message), 0, message.length, this, false);
+        sm, -1, Uint8List.fromList(message), 0, message.length, Uint8List.fromList(this + verifyKey), true);
     if (result != 0) {
       throw Exception('Signing the massage is failed');
     }
@@ -80,15 +79,29 @@ class CardanoExtendedPrivateKey extends ByteList
 
 }
 
+class CardanoBip32PublicKey extends CardanoExtendedPublicKey implements ExtendedPublicKey {
+  ExtendedKey get public => this;
+  ByteList get chainCode => suffix;
+  ExtendedKey derive(int index);
+}
+
+class CardanoBip32PrivateKey extends CardanoExtendedPrivateKey implements ExtendedPrivateKey {
+  ExtendedKey get public =>  this.publicKey;
+  ByteList get chainCode => suffic;
+  ExtendedKey derive(int index);
+}
+
+
 void main() {
+  // Test 1
   var esk = CardanoExtendedPrivateKey.decode(
       'ed25519e_sk1vz4jdwnehsx39zdj6c5n5c9q7r0gp77naw0226m7452ahvckxdvy6tfkllwl8fhpg5mt2akwkc7su4xy26ysn5qfy9jfne5uqfueljsqvy4us');
   var epk = CardanoExtendedPublicKey.decode(
       'ed25519_pk1wh6mye86aypjy0qlgndszk9txf8aw802wmct3xmm9yh935jhcukq60sp6l');
 
-  var message = 'Hello world!';
+  var message = 'Hello world!'.codeUnits;
 
-  var signature = esk.sign(message.codeUnits);
+  var signature = esk.sign(message);
   print(esk.encode(HexCoder.instance));
 
   print(signature.toString());
@@ -97,4 +110,13 @@ void main() {
   var epk1 = esk.publicKey;
   print(epk.encode());
   print(epk1.encode());
+
+  // Test 2
+  // mnemonic: art forum devote street sure rather head chuckle guard poverty release quote oak craft enemy
+  // entropy: [0x0c, 0xcb, 0x74, 0xf3, 0x6b, 0x7d, 0xa1, 0x64, 0x9a, 0x81, 0x44, 0x67, 0x55, 0x22, 0xd4, 0xd8, 0x09, 0x7c, 0x64, 0x12];
+  // xprv1hretan5mml3tq2p0twkhq4tz4jvka7m2l94kfr6yghkyfar6m9wppc7h9unw6p65y23kakzct3695rs32z7vaw3r2lg9scmfj8ec5du3ufydu5yuquxcz24jlkjhsc9vsa4ufzge9s00fn398svhacse5su2awrw
+  // xpub1eamrnx3pph58yr5l4z2wghjpu2dt2f0rp0zq9qquqa39p52ct0xercjgmegfcpcdsy4t9ld90ps2epmtcjy3jtq77n8z20qe0m3pnfqntgrgj
+  // sk: b8f2bece9bdfe2b0282f5bad705562ac996efb6af96b648f4445ec44f47ad95c10e3d72f26ed075422a36ed8585c745a0e1150bcceba2357d058636991f38a3791e248de509c070d812ab2fda57860ac876bc489192c1ef4ce253c197ee219a4
+  // pk: cf76399a210de8720e9fa894e45e41e29ab525e30bc402801c076250d1585bcd91e248de509c070d812ab2fda57860ac876bc489192c1ef4ce253c197ee219a4
+  // cc: 91e248de509c070d812ab2fda57860ac876bc489192c1ef4ce253c197ee219a4
 }
