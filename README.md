df86f273de325cbd905c2e2f82e07341f038412c
Pal Dorogi <pal.dorogi@gmail.com>
2021-01-03 18:06:02 +1000

    Update README.md

--- README.md
+++ README.md
@@ -1,2 +1,70 @@
 # bip32-ed25519-dart
 The Dart implementation of the BIP32-Ed25519 the deterministic key generation solution for the Ed25519 curve
+
+
+# API Interfaces
+
+TBD
+# Key Types
+- ED25519 (RFC 8032) Keys 
+    - the private key is a 32-bytre long cryptographically secure random data.
+- ED25519 Extended Keys
+    - the private key is a 64-byte long data, and it can be interpreted as the SHA512 hashed and clear/set bit of an ED25519 private key.
+    - they also can be interpreted as standalone keys, though brute-force is required for retrieving the ED25519 private key from an extended key.
+- BIP32-ED25519 Keys
+    - BIP32-ED25519 derivation requires that the 3rd bit of the 31th bytes of an ED25519 Extended key must be cleared.
+    - This causes half of the BIP32-ED25519 keys are not backward compatible /w ED25519 and ED25519 Extended keys.
+
+
+### ED25519 Keys
+
+The [ed25519](http://ed25519.cr.yp.to/) is an 
+[Elliptic Curve Digital Signature Algortithm](http://en.wikipedia.org/wiki/Elliptic_Curve_DSA) using curve25519 by [Dan Bernstein](http://cr.yp.to/djb.html), 
+[Niels Duif](http://www.nielsduif.nl/), 
+[Tanja Lange](http://hyperelliptic.org/tanja), 
+[Peter Schwabe](http://www.cryptojedi.org/users/peter/), 
+and [Bo-Yin Yang](http://www.iis.sinica.edu.tw/pages/byyang/).
+
+The key is 64-byte long and contains the the 32-byte long seed a.k.a private key that is used for 
+generate the secret key and public key.
+
+### ED25519 Extended keys
+
+The 64-byte long extended keys contains either 
+    - only the 64-byte long secret key.
+    - or the 64-byte long extended private key and the 32-byte public key similar to the normal 
+    ED25519 key whihc contains the 32-byte private key (seed) and the 32-byte long private key.
+
+Though, `pinenacl-dart`'s extended interface expecting a concatenated secret and public key.
+It's due to the assumption that the public key is already known (no scalar_base multiplication is needed for retrieveing the key)
+
+The [message signing and signature verifying is compatible /w ED25519](https://raw.githubusercontent.com/RubyCrypto/ed25519/master/ed25519.png).
+
+
+### BIP32-ED25519 Keys
+
+The 96-byte long BIP32-ED25519 keys contains the ed25519e_sk and the chain code.
+The message signing and signature verifying is compatible /w ED25519.
+
+Note: 
+
+
+# Restrictions
+
+In Cardano blockchain, the keys are derived by the `BIP32-ED25519` specification.
+
+The `BIP32-ED25519`, in addition to the `ED25519` [(RFC 8032)](https://tools.ietf.org/html/rfc8032), needs the 3rd bit cleared of the 31th byte.
+
+Therefore, the half of the `ED25519 Extended` secret keys and therefore the half of the  `ED25519` private keys are not compatible /w `BIP32-ED25519`.
+
+To overcome of this restriction, different wallet implementation decided to generate their master node/root key differently (as BIP32-ED25519 specification only requires that bit cleared on the master node/root key as the derived keys would have that bit cleared in the derivation functions anyway). 
+
+Some of them (such as Yoroi), just clear that `additional` 3rd bit, while others (such as the old Daedalus) are hashing the corresponding master key until they find a compatible key, and when it's found, they set and clear the bits as specified in the `RFC 8032`.
+
+ ## Resolution
+
+There are different type of resolutions. The most proper way would be: generate only a `BIP32-ED25519` compatible 24-word mnemonics and therefore a 256-bit long master secret for new wallets and discard others.
+Then use that 256-bit master secret as `k` specified in `BIP32-ED25519`.
+
+Drawback of this is that half of the already existing user's mnemonics are not compatible, therefore
+they need either to move to a new wallet or using some out-dated master-key generation algorithm.
--- lib/bip32_ed25519.dart
+++ lib/bip32_ed25519.dart
@@ -1,9 +1,11 @@
 import 'dart:typed_data';
 import 'package:pinenacl/api.dart';
 
-mixin ExtendedKey on ByteList {
+mixin ExtendedKey on Suffix {
   ExtendedKey get public;
   ByteList get chainCode => suffix;
+  ByteList get key => prefix;
+
   ExtendedKey derive(int index);
 }
 
@@ -19,8 +21,7 @@ mixin ExtendedPublicKey implements ExtendedKey {}
 abstract class ChainCode {}
 
 // This is a simple ED25519 Public Key.
-class CardanoExtendedPublicKey extends VerifyKey
-    implements Verify {
+class CardanoExtendedPublicKey extends VerifyKey implements Verify {
   CardanoExtendedPublicKey(List<int> list) : super(list);
 
   factory CardanoExtendedPublicKey.decode(String data,
@@ -31,6 +32,7 @@ class CardanoExtendedPublicKey extends VerifyKey
 }
 
 class CardanoExtendedPrivateKey extends ByteList
+    with Suffix
     implements AsymmetricPrivateKey, Sign {
   CardanoExtendedPrivateKey._fromValidBytes(List<int> secret)
       : verifyKey = _toPublic(secret),
@@ -68,8 +70,9 @@ class CardanoExtendedPrivateKey extends ByteList
   SignedMessage sign(List<int> message) {
     // signed message
     var sm = Uint8List(message.length + TweetNaCl.signatureLength);
-    final result = TweetNaCl.crypto_sign(
-        sm, -1, Uint8List.fromList(message), 0, message.length, Uint8List.fromList(this + verifyKey), true);
+    final result = TweetNaCl.crypto_sign(sm, -1, Uint8List.fromList(message), 0,
+        message.length, Uint8List.fromList(this.prefix + verifyKey),
+        extended: true);
     if (result != 0) {
       throw Exception('Signing the massage is failed');
     }
@@ -77,27 +80,79 @@ class CardanoExtendedPrivateKey extends ByteList
     return SignedMessage.fromList(signedMessage: sm);
   }
 
+  @override
+  int prefixLength = 64;
 }
 
-class CardanoBip32PublicKey extends CardanoExtendedPublicKey implements ExtendedPublicKey {
+class CardanoBip32PublicKey extends CardanoExtendedPublicKey
+    with Suffix
+    implements ExtendedPublicKey {
+  CardanoBip32PublicKey(List<int> list) : super(list);
+
   ExtendedKey get public => this;
   ByteList get chainCode => suffix;
-  ExtendedKey derive(int index);
+  ExtendedKey derive(int index) {
+    return CardanoBip32PublicKey([]);
+  }
+
+  factory CardanoBip32PublicKey.decode(String data, {Encoder coder = decoder}) {
+    final decoded = coder.decode(data);
+    return CardanoBip32PublicKey(decoded);
+  }
+  static const decoder = Bech32Coder(hrp: 'xpub');
+
+  @override
+  Encoder get encoder => decoder;
+
+  @override
+  int prefixLength = 32;
+
+  @override
+  ByteList get key => prefix;
 }
 
-class CardanoBip32PrivateKey extends CardanoExtendedPrivateKey implements ExtendedPrivateKey {
-  ExtendedKey get public =>  this.publicKey;
-  ByteList get chainCode => suffic;
-  ExtendedKey derive(int index);
-}
+class CardanoBip32PrivateKey extends CardanoExtendedPrivateKey
+    with Suffix
+    implements ExtendedPrivateKey {
+  CardanoBip32PrivateKey(List<int> list) : super._fromValidBytes(list);
+  ExtendedKey get public =>
+      CardanoBip32PublicKey(this.publicKey + this.chainCode);
+  ByteList get chainCode => suffix;
+  ExtendedKey derive(int index) {
+    return CardanoBip32PrivateKey([]);
+  }
 
+  factory CardanoBip32PrivateKey.decode(String data,
+      [Encoder defaultDecoder = decoder]) {
+    final decoded = defaultDecoder.decode(data);
+    return CardanoBip32PrivateKey(decoded);
+  }
+
+  static const decoder = Bech32Coder(hrp: 'xprv');
+
+  @override
+  Encoder get encoder => decoder;
+
+  @override
+  int prefixLength = 64;
+
+  @override
+  ByteList get key => prefix;
+}
 
 void main() {
   // Test 1
-  var esk = CardanoExtendedPrivateKey.decode(
-      'ed25519e_sk1vz4jdwnehsx39zdj6c5n5c9q7r0gp77naw0226m7452ahvckxdvy6tfkllwl8fhpg5mt2akwkc7su4xy26ysn5qfy9jfne5uqfueljsqvy4us');
-  var epk = CardanoExtendedPublicKey.decode(
-      'ed25519_pk1wh6mye86aypjy0qlgndszk9txf8aw802wmct3xmm9yh935jhcukq60sp6l');
+  var esk = CardanoBip32PrivateKey.decode(
+      'xprv1hretan5mml3tq2p0twkhq4tz4jvka7m2l94kfr6yghkyfar6m9wppc7h9unw6p65y23kakzct3695rs32z7vaw3r2lg9scmfj8ec5du3ufydu5yuquxcz24jlkjhsc9vsa4ufzge9s00fn398svhacse5su2awrw');
+  //  'ed25519e_sk1vz4jdwnehsx39zdj6c5n5c9q7r0gp77naw0226m7452ahvckxdvy6tfkllwl8fhpg5mt2akwkc7su4xy26ysn5qfy9jfne5uqfueljsqvy4us');
+  var epk = CardanoBip32PublicKey.decode(
+      'xpub1eamrnx3pph58yr5l4z2wghjpu2dt2f0rp0zq9qquqa39p52ct0xercjgmegfcpcdsy4t9ld90ps2epmtcjy3jtq77n8z20qe0m3pnfqntgrgj');
+  //  'ed25519_pk1wh6mye86aypjy0qlgndszk9txf8aw802wmct3xmm9yh935jhcukq60sp6l');
+
+  print('AAAAAA: ' + esk.public.encode(HexCoder.instance));
+  print('BBBBBB: ' + epk.encode(HexCoder.instance));
+  print('CCCCCC: ' + esk.public.chainCode.encode(HexCoder.instance));
+  print('DDDDDD: ' + epk.chainCode.encode(HexCoder.instance));
 
   var message = 'Hello world!'.codeUnits;
 
